29af54b8b46c900f48e49c349162e83619de37c5
SyedSaifuddin045 <syedsaifuddin.vlcro@gmail.com>
2023-08-09 12:21:53 +0530

    Added Automatic deletion of file after sending

--- server.js
+++ server.js
@@ -28,19 +28,19 @@ server.listen(PORT, () => {
 
 io.on('connection', (socket) => {
   console.log('Client connected');
-  
+
   socket.on('GetFilter', (input) => {
     const pyshell = new PythonShell('GetFilters.py', {
       mode: 'json',
-      args:['-st',input]
+      args: ['-st', input]
     });
-  
+
     pyshell.on('json', (jsonString) => {
       const filterJson = JSON.parse(jsonString);
       console.log(filterJson)
       socket.emit('filterResult', filterJson);
     });
-  
+
     pyshell.end((err) => {
       if (err) {
         console.error(err);
@@ -48,10 +48,10 @@ io.on('connection', (socket) => {
       }
     });
   });
-  
+
 
   socket.on('StartScript', (input) => {
-    const pyshell = new PythonShell('ScrapeAmazon.py', { mode: 'text', pythonOptions: ['-u'],args:['-st',input] })
+    const pyshell = new PythonShell('ScrapeAmazon.py', { mode: 'text', pythonOptions: ['-u'], args: ['-st', input] })
     console.log("Started running pyhton script")
     pyshell.on('message', (message) => {
       console.log("Message :" + message)
@@ -63,16 +63,27 @@ io.on('connection', (socket) => {
       }
       else {
         console.log('Script Execution Finished');
-        fs.readFile(`${input}.json`,'utf-8',(err,data)=>{
-          if(err)
-          {
-            console.log("Error reading Json file."+err);
+
+        const filePath = `${input}.json`;
+
+        fs.readFile(filePath, 'utf-8', (err, data) => {
+          if (err) {
+            console.log("Error reading JSON file: " + err);
+          } else {
+            socket.emit('json', data, (acknowledgment) => {
+              if (acknowledgment == 'success') {
+                // After sending the data, delete the file
+                fs.unlink(filePath, (deleteErr) => {
+                  if (deleteErr) {
+                    console.log("Error deleting file: " + deleteErr);
+                  } else {
+                    console.log("File deleted successfully.");
+                  }
+                });
+              }
+            });
           }
-          else
-          {
-            socket.emit('json',data)
-          }
-        })
+        });
       }
     });
 
